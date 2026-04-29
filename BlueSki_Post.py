ba379bc3405bf617e73001a05f68e2836edb4372
Lkarm003 <lkarm003@ucr.edu>
2026-04-28 23:52:03 -0700

    API Collection

---
+++ BlueSki_Post.py
@@ -0,0 +1,5 @@
+from atproto import Client
+
+client = Client()
+client.login('lahari.karmuchi@gmail.com', 'w4pm-vmf4-t5cn-ym2g')
+post = client.send_post('Hello world! I posted this via the Python SDK.')
