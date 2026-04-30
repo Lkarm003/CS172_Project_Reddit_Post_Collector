1231ea4fcbf4773679741eb49006b90934268882
jasminehanna10 <jasmine.hanna2005@gmail.com>
2026-04-30 16:50:37 -0700

    Remove BlueSki_Post.py. Not relevant anymore and does nothing

--- BlueSki_Post.py
+++
@@ -1,5 +0,0 @@
-from atproto import Client
-
-client = Client()
-client.login('lahari.karmuchi@gmail.com', 'w4pm-vmf4-t5cn-ym2g')
-post = client.send_post('Hello world! I posted this via the Python SDK.')
